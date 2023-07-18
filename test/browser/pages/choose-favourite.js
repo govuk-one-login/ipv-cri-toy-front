c1f690ca6587dfa29c85a7b47b26ec57487acc6f
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2023-07-18 13:21:53 +0100

    fix: Use /toy route for toy pages
    - All journeys need to start via the /oauth route
    - This journey is then redirected to the start of the toy router
    - Allowing the default / route makes the entry point for tests more
      confusing, and has lead to bugs in the past
    - 404ing on the default / route and using /toy instead removes the
      opportunity for these buggy tests

--- test/browser/pages/choose-favourite.js
+++ test/browser/pages/choose-favourite.js
@@ -4,7 +4,7 @@ module.exports = class PlaywrightDevPage {
    */
   constructor(page) {
     this.page = page;
-    this.path = "/choose-favourite";
+    this.path = "/toy/choose-favourite";
   }
 
   async continue() {
--- test/browser/pages/intro.js
+++ test/browser/pages/intro.js
@@ -4,7 +4,7 @@ module.exports = class PlaywrightDevPage {
    */
   constructor(page) {
     this.page = page;
-    this.path = "/intro";
+    this.path = "/toy/intro";
   }
 
   async continue() {
